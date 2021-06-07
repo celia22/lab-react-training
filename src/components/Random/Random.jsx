3208f09d544edcdeca60670f04c91930875f5455
celia22 <celialopez22@gmail.com>
2021-06-07 17:45:13 +0200

    refactor IT 3

--- src/components/Random/Random.jsx
+++ src/components/Random/Random.jsx
@@ -1,9 +1,7 @@
 import './Random.css';
 
 const Random = (props) => {
-  let min = props.min;
-  let max = props.max;
-  const randomNumber = Math.floor(Math.random() * max) + min
+   const randomNumber = Math.floor(Math.random() * props.max) + props.min
   return(
     <div className="random">
       <h4>Random value between {props.min} and {props.max} => {randomNumber} </h4>
