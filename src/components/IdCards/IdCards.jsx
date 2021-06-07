2b32aa84310d4edbd1ff55d76eb9196468fc36f4
celia22 <celialopez22@gmail.com>
2021-06-07 09:40:19 +0200

    refactor IT 1

--- src/components/IdCards/IdCards.jsx
+++ src/components/IdCards/IdCards.jsx
@@ -3,18 +3,17 @@ import './idCards.css';
 
 const IdCard = (props) => {
   
-  const month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-  const day = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+  const birth2String = birthDate => birthDate.toDateString();
   
   return(
     <div className="idCard">
            <img src={props.picture} alt="IDcard"/>
            <div className="data_container">
-            <p><strong>First name: </strong>{props.firstName}</p>
-            <p><strong>Last name:</strong> {props.lastName}</p>
-            <p><strong>Gender:</strong> {props.gender}</p>
-            <p><strong>Height: </strong>{props.height/100}m</p>
-            <p><strong>Birth:</strong> {day[props.birth.getDay()]} {month[props.birth.getMonth()]} {props.birth.getDate()} {props.birth.getFullYear()}</p>
+            <p><b>First name: </b>{props.firstName}</p>
+            <p><b>Last name:</b> {props.lastName}</p>
+            <p><b>Gender:</b> {props.gender}</p>
+            <p><b>Height: </b>{props.height/100}m</p>
+            <p><b>Birth</b>: {birth2String(props.birth)}</p>
             </div> 
         </div>
     )  
--- src/components/Rating/Rating.jsx
+++ src/components/Rating/Rating.jsx
@@ -1,11 +1,10 @@
 const Rating = (props) => {
   const rounded = Math.round(props.children)
   const whiteStar = (5 - rounded) 
-  const blackStar = rounded 
-  
+    
   return(
     <div>
-      <p>{"☆".repeat(whiteStar)} {"★".repeat(blackStar)} </p>     
+      <p>{"☆".repeat(whiteStar)} {"★".repeat(rounded)} </p>     
     </div>
   )
 }
